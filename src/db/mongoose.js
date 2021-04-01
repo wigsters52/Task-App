0ad045f20fbf521c135542dd0b18761c3f95e883
Ryan Wiggins <ryanwiggins52@gmail.com>
2021-04-01 18:49:44 -0400

    messing aroudn with validation

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true,
@@ -7,20 +8,56 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 
 const User = mongoose.model('User', {
     name: {
-        type: String
+        type: String,
+        required: true
     }, 
     age: {
-        type: Number
+        type: Number, 
+        validate(value){
+            if(value < 0) {
+                throw new Error('Age must be a positive number')
+            }
+        }
+    }, 
+    email: {
+        type: String, 
+        required: true, 
+        validator(value){
+            if(!validator.isEmail(value)){
+                throw new Error('Email is invalid')
+            }
+        }
+    }
+
+})
+
+// const ryan = new User({
+//     name: 'Ryan',
+//     age: 30
+// })
+
+// ryan.save().then(() => {
+//     console.log(ryan)
+// }).catch((error) => {
+//     console.log("error!", error)
+// })
+
+const Task = mongoose.model('Task', {
+    description: {
+        type: String
+    }, 
+    completed: {
+        type: Boolean
     }
 })
 
-const ryan = new User({
-    name: 'Ryan',
-    age: 30
+const task1 = new Task({
+    description: 'Take ted out for a walk',
+    completed: false
 })
 
-ryan.save().then(() => {
-    console.log(ryan)
-}).catch((error) => {
-    console.log("error!", error)
-})
+task1.save().then(() => {
+        console.log(task1)
+    }).catch((error) => {
+        console.log("error!", error)
+    })
