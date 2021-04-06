9fe18397ffc95e06c0cd48d143a8bc6b9384d1f7
Ryan Wiggins <ryanwiggins52@gmail.com>
2021-04-06 10:13:00 -0400

    task endpoint created

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -7,17 +7,5 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 })
 
 
-const Task = mongoose.model('Task', {
-    description: {
-        type: String, 
-        required: true, 
-        trim: true
-    }, 
-    completed: {
-        type: Boolean,
-        required: false,
-        default: true
-        
-    }
-})
+
 
---
+++ src/models/task.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+
+const Task = mongoose.model('Task', {
+    description: {
+        type: String, 
+        required: true, 
+        trim: true
+    }, 
+    completed: {    
+        type: Boolean,
+        required: false,
+        default: true
+        
+    }
+})
+
+module.exports = Task
