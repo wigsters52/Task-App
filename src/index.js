f12bb70e5c5ba00c21f87c56100a0a05840c9dbe
Ryan Wiggins <ryanwiggins52@gmail.com>
2021-04-12 12:54:00 -0400

    adding more  patch endpoints

--- src/index.js
+++ src/index.js
@@ -51,7 +51,7 @@ app.patch('/users/:id', async (req, res) => {
     if(!isValidOperation){
         return res.status(400).send({ error: 'Invalid updates!'})
     }
-    
+
     try {
         const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true})
 
@@ -95,6 +95,27 @@ app.get('/tasks/:id', async (req, res) => {
         res.send(task)
     } catch(e) {
         res.status(500).send()
+    }
+})
+
+app.patch('/tasks/:id', async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['description', 'completed']
+    const isValidOperation = updates.every((update) =>  allowedUpdates.includes(update))
+
+    if(!isValidOperation){
+        return res.status(400).send({ error: 'Invalid updates!'})
+    }
+    
+    try {
+        const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true})
+
+        if(!task){
+            return res.status(404).send()
+        }
+        res.send(task)
+    } catch(e){
+        res.status(400).send(e)
     }
 })
 
